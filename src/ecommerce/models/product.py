b300872cf6dd2dc1b5f1152ef813acf1f567bffc
mustafa <mustafagamaldeveloper@gmail.com>
2020-11-22 20:45:06 +0200

    Modify Category model; Create get_most_sold_products method, Modify Subcategory model; Create get_most_sold_products method

--- src/ecommerce/models/product.py
+++ src/ecommerce/models/product.py
@@ -10,18 +10,30 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_most_sold_products(self):
+        products = []
+
+        for subcategory in self.subcategories.all():
+            products.extend(subcategory.get_most_sold_products())
+
+        return products
+
 
 class Subcategory(models.Model):
     name = models.CharField(max_length=150)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="subcategories")
 
     def __str__(self):
         return f"{self.category.name} => {self.name}"
 
+    def get_most_sold_products(self):
+        products = self.products.order_by('-sold')[:15]
+        return list(products)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=150)
-    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, related_name="products")
     price = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     image = models.ImageField(upload_to="product_images", default="default.jpg")
     available = models.PositiveIntegerField(default=0)

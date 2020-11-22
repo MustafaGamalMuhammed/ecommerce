984afedf9cad312adb15b96bd65e412186d8b41b
mustafa <mustafagamaldeveloper@gmail.com>
2020-11-22 19:39:07 +0200

    Create signup, signin, home, shop, product, cart views and templates, Create base.html and base.css

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -1,6 +1,31 @@
 from django.urls import path
+from ecommerce import views
 
 
-urlpatterns = [
-    
+authentication_urlpatterns = [
+    path('signup/', views.signup, name="signup"),
+    path('signin/', views.signin, name="signin"),
 ]
+
+home_urlpatterns = [
+    path('', views.home, name="home"),
+]
+
+shop_urlpatterns = [
+    path('shop/', views.shop, name="shop"),
+]
+
+product_urlpatterns = [
+    path('product/', views.product, name="product"),
+]
+
+cart_urlpatterns = [
+    path('cart/', views.cart, name="cart"),
+]
+
+urlpatterns = []
+urlpatterns.extend(authentication_urlpatterns)
+urlpatterns.extend(shop_urlpatterns)
+urlpatterns.extend(product_urlpatterns)
+urlpatterns.extend(cart_urlpatterns)
+urlpatterns.extend(home_urlpatterns)
---
+++ src/ecommerce/views/__init__.py
@@ -0,0 +1,6 @@
+from .signup import signup
+from .signin import signin
+from .home import home
+from .shop import shop
+from .product import product
+from .cart import cart

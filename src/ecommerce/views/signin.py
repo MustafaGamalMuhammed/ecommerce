b8e076890c2b88e1f322ab4322d69d026b15092a
mustafa <mustafagamaldeveloper@gmail.com>
2020-11-23 18:34:46 +0200

    Modify signin and signup view; apply authentication functionalities, Modify signin.html and signup.html templates; add forms

--- src/ecommerce/views/signin.py
+++ src/ecommerce/views/signin.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 import django.contrib.auth as auth
+from django.contrib import messages
 
 
 def signin(request):
-    return render(request, 'ecommerce/signin.html')
+    if request.method == "POST":
+        print(request.POST)
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = auth.authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Wrong username or password')
+        else:
+            for error in form.errors:
+                messages.error(request, form.errors[error])
+
+    context = {
+        'form': AuthenticationForm(),
+    }
+    return render(request, 'ecommerce/signin.html', context=context)
 
 
 def logout(request):
--- src/ecommerce/views/signup.py
+++ src/ecommerce/views/signup.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from ecommerce.forms import SignupForm
 
 
 def signup(request):
-    return render(request, 'ecommerce/signup.html')
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('signin')
+        else:
+            for error in form.errors:
+                messages.error(request, form.errors[error])
+
+    context = {
+        'form': SignupForm(),
+    }
+    return render(request, 'ecommerce/signup.html', context=context)

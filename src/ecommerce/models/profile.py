1570ddd5e49389d273cbf60f33abb0fc3e317c54
mustafa <mustafagamaldeveloper@gmail.com>
2020-11-29 17:26:09 +0200

    Modify Profile model; add products field

--- src/ecommerce/models/profile.py
+++ src/ecommerce/models/profile.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ecommerce.models import Cart, Product
+from .cart import Cart
+from .product import Product
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     cart = models.OneToOneField(Cart, on_delete=models.SET_NULL, null=True, blank=True)
     likes = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product, related_name="seller")
 
     def __str__(self):
         return f"{self.user.username}'s profile"
